3d546c1bd1679ea39bb0d3081688d5a5339fbe4a
JIAQI13 <jiaqi11@ualberta.ca>
2021-06-25 03:28:07 +0000

    add counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,12 @@ class Admin::DashboardController < ApplicationController
   :password => ENV['PASSWORD']
   }
   http_basic_authenticate_with name:Rails.configuration.a[:name], password:Rails.configuration.a[:password]
-  def show
+  def show_products
+    @count_products = Product.count
   end
+  def show_categories
+    @count_categories = Category.count
+  end
+  helper_method :show_products
+  helper_method :show_categories
 end
