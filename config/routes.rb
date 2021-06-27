3f9f5ee3caa63c1af311b7f81824db5f4802960a
JIAQI13 <jiaqi11@ualberta.ca>
2021-06-27 21:56:20 +0000

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-  get 'about/' => 'about#index'
+  get '/about' => 'about#index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
 
   resource :cart, only: [:show] do
